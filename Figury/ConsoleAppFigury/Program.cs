3bed2f11558405e9f761c41cf6cb728bfd3604c4
Mikołaj Strusiński <mikistrus@gmail.com>
2020-03-02 20:27:32 +0100

    Main Commit

---
+++ Figury/ConsoleAppFigury/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiguryLibrary; //dołączony namespace
+
+namespace ConsoleAppFigury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trojkat t1; //deklaracja zmiennej typu Trojkat
+            t1 = new Trojkat(); //new używamy zawsze do tworzenia obiektu
+            Trojkat t2 = new Trojkat(-1, 2, 1);
+            var t3 = new Trojkat(c: 1, b: 2, a: 2); //inny sposób na tworzenie obiektu
+           //^zamiast pisać trójkąt to to jest oczywiste że var jest trójkątem, krótszy zapis
+
+
+
+            Console.WriteLine(t1);
+            Console.WriteLine($"obwód: {t1.GetObwod()}"); //uzycie zmiennej liczącej obwód
+            Console.WriteLine($"obwód: {t1.Obwod}");      //uzycie property o nazwie obwód
+            Console.WriteLine($"Pole: {t1.GetPole()}");   
+            Console.WriteLine($"Pole: {t1.Pole}");
+            try
+            {
+                t1.A = -1;
+                t1.B = -2;
+                t1.C = -3;
+            }   
+            catch(ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("Nie wolno bokom przypisywać wartości ujemnych");
+            }
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Pole);
+
+            Console.WriteLine("===============================================");
+            Console.WriteLine(t2);
+
+
+
+            
+
+    }
+    }
+}
---
+++ Figury/FiguryLibrary/trojkat.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace FiguryLibrary
+{
+    public class Trojkat
+    {
+        //dane obniektu (zmienne)
+        private double a; //this.a
+        public double A
+        {
+            get { return a; }
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("wartość a musi być dodatnia");
+                else a = value;       
+            }
+        }
+       
+        private double b; //this.b
+        public double B
+        {
+            get => b; //można krócej zamiast { return a; }
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("wartość b musi być dodatnia");
+                else b = value;
+            }
+        }
+        private double c; //this.c
+        public double C
+        {
+            get => c; //można krócej zamiast { return a; }
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException("wartość c musi być dodatnia");
+                else c = value;
+            }
+        }
+
+
+
+
+
+
+        // konstruktory
+        public Trojkat() //konstruktor domyślny (bez argumentów)
+        {
+            a = b = c = 1.0;
+        }
+        public Trojkat(double a, double b, double c)
+        {
+            if (a + b <= c || a + c <= b || b + c <= a) //jeżeli choć jedna z tych kombinacji boków będzie true to wszystko będzie true
+            {
+                throw new ArgumentException("niespełniony warunek trójkąta");
+            }
+
+            A = a; //(wcześniej było this.a tutaj) this.a zawsze będzie zmienną z klasy
+            B = b;
+            C = c;
+        }
+
+
+        //tekstowa reprezentacja obiektu
+        public override string ToString()
+        {
+            return $"Trojkąt(a: {a},b: {b},c: {c})";
+        }
+
+
+        //właściwości i metody
+        public double GetObwod()            //java style
+        {
+               return a + b + c; 
+        }
+
+        //property zwracające obwód tego trójkąta
+        //piszemy w jednej liniice bo kod jest prosty
+        public double Obwod => a + b + c;   //C# style
+                
+        //piszemy to w metodzie bo kod jest skomplikowany
+        public double GetPole() //java style
+        {
+            var p = 0.5 * GetObwod();
+            var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return s;
+        }
+
+        public double Pole // C# style
+        {
+            get
+            {
+                var p = 0.5 * Obwod;
+                var s = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+                return s;
+            }
+            
+        }
+
+    }
+
+
+}
